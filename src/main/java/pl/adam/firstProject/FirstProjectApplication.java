d66c121f19d3379ba47eb11969dc35a1eaf38c91
Adam Waniczek <adam.waniczek@gmail.com>
2020-05-13 09:56:07 +0200

    init project

---
+++ src/main/java/pl/adam/firstProject/FirstProjectApplication.java
@@ -0,0 +1,14 @@
+package pl.adam.firstProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjectApplication {
+
+	public static void main(String[] args) {
+		//moment w którym startujemy spring app
+		SpringApplication.run(FirstProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/adam/firstProject/FirstProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.adam.firstProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
