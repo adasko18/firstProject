4011c05e08fdd62d31f8448bcde4292a27615b53
Adam Waniczek <adam.waniczek@gmail.com>
2020-06-16 10:59:21 +0200

    add getPage test for all controllers

---
+++ src/test/java/pl/adam/firstProject/controller/LoginControllerTest.java
@@ -0,0 +1,33 @@
+package pl.adam.firstProject.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.ui.Model;
+import pl.adam.firstProject.service.LoginService;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@WebMvcTest(LoginController.class)
+class LoginControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private LoginService loginService;
+
+    @Test
+    void shouldSayStatusAndViewNameIsOk() throws Exception {
+        mockMvc.perform(get("/login"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("login"));
+    }
+
+}
